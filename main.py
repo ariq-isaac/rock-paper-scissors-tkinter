56e0d410a2240dcb1a0464b2024ddd711653565a
Ariq Isaac <2009ariq@gmail.com>
2026-09-20 13:37:38 +0800

    Add Classes to Widgets, Buttons, And Events

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 import tkinter as tk
 import random
 
-# Initialize tkinter
-window = tk.Tk()
-
-window.title("Rock Paper Scissors")
-window.geometry("300x200")
-
-message = tk.Label(window, text="Your Input:")
-message.pack()
-
+# ----- GAME LOGIC -----
 
 # Gets a random choice between rock, paper, or scissors
 def get_computer_choice():
@@ -22,54 +14,79 @@ wins = {
     "scissors": "rock"
 }
 
-#---Initialize the button declaration---
-buttons_frame = tk.Frame(window)
+# ----- GRAPHICAL USER INTERFACE -----
 
-rock_button = tk.Button(buttons_frame, text="Rock")
-rock_button.grid(row=0, column=0, padx=10, pady=10)
+# Initialize tkinter widgets
+class Widgets:
+    def __init__(self):
+        self.window = tk.Tk()
 
-paper_button = tk.Button(buttons_frame, text="Paper")
-paper_button.grid(row=0, column=1, padx=10, pady=10)
+        self.window.title("Rock Paper Scissors")
+        self.window.geometry("300x200")
 
-scissors_button = tk.Button(buttons_frame, text="Scissors")
-scissors_button.grid(row=0, column=2, padx=10, pady=10)
+        self.message = tk.Label(self.window, text="Your Input:")
+        self.message.pack()
 
-# Packs button_frame
-buttons_frame.pack()
+widgets = Widgets()
 
-#---Creates the frame for the result---
-result_frame = tk.Frame(window)
+#Initialize the button declaration
+class Buttons:
+    def __init__(self):
+        self.buttons_frame = tk.Frame(widgets.window)
+
+        self.rock_button = tk.Button(self.buttons_frame, text="Rock")
+        self.rock_button.grid(row=0, column=0, padx=10, pady=10)
+
+        self.paper_button = tk.Button(self.buttons_frame, text="Paper")
+        self.paper_button.grid(row=0, column=1, padx=10, pady=10)
+
+        self.scissors_button = tk.Button(self.buttons_frame, text="Scissors")
+        self.scissors_button.grid(row=0, column=2, padx=10, pady=10)
+
+        # Packs button_frame
+        self.buttons_frame.pack()
+
+buttons = Buttons()
+
+# Creates a frame and message to display the result to the user
+class GameResult:
+    def __init__(self):
+        #---Creates the frame for the result---
+        self.result_frame = tk.Frame(widgets.window)
     
-# Creates an empty label to be asigned later
-round_result = tk.Label(result_frame)
-round_result.pack()
+        # Creates an empty label to be asigned later
+        self.round_result = tk.Label(self.result_frame)
+        self.round_result.pack()
     
-result_frame.pack()
+        self.result_frame.pack()
+
+game_result = GameResult()
 
 # Creates an event for a button click
-def on_click(event):
-    # Runs get_computer_choice() and name is as computer_choice
-    computer_choice = get_computer_choice()
+class WidgetEvents:
+    def __init__(self, event):
+        # Runs get_computer_choice() and name is as computer_choice
+        self.computer_choice = get_computer_choice()
 
-    # Gets the text of the button and name is as human_choice
-    button = event.widget
-    human_choice = button.cget("text").lower()
+        # Gets the text of the button and name is as human_choice
+        self.button = event.widget
+        self.human_choice = self.button.cget("text").lower()
 
-    # Runs conditional checking to identify the winner
-    if human_choice == computer_choice:
-        output = "It's a draw!"
-    elif wins[human_choice] == computer_choice:
-        output = f"You Won!\nYou: {human_choice.capitalize()}, Computer: {computer_choice.capitalize()}"
-    else:
-        output = f"You Lost!\nYou: {human_choice.capitalize()}, Computer: {computer_choice.capitalize()}"
+        # Runs conditional checking to identify the winner
+        if self.human_choice == self.computer_choice:
+            output = "It's a draw!"
+        elif wins[self.human_choice] == self.computer_choice:
+            output = f"You Won!\nYou: {self.human_choice.capitalize()}, Computer: {self.computer_choice.capitalize()}"
+        else:
+            output = f"You Lost!\nYou: {self.human_choice.capitalize()}, Computer: {self.computer_choice.capitalize()}"
 
-    round_result.config(text=output)
-    
+        game_result.round_result.config(text=output)
 
 # Listens for event to the buttons
-rock_button.bind("<Button-1>", on_click)
-paper_button.bind("<Button-1>", on_click)
-scissors_button.bind("<Button-1>", on_click)
+buttons.rock_button.bind("<Button-1>", WidgetEvents)
+buttons.paper_button.bind("<Button-1>", WidgetEvents)
+buttons.scissors_button.bind("<Button-1>", WidgetEvents)
 
 # Runs the window
-window.mainloop()
+if __name__ == "__main__":
+    widgets.window.mainloop()
